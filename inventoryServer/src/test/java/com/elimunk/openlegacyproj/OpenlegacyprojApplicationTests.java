c53132315ed8f74c07f297efec6f64f3d2ea8d3c
Eli Munk <43830882+elimunk@users.noreply.github.com>
2019-11-18 15:48:30 -0800

    Server

---
+++ inventoryServer/src/test/java/com/elimunk/openlegacyproj/OpenlegacyprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elimunk.openlegacyproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenlegacyprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
